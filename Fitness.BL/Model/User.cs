c49d8b981622125364cbc53863e8083839e415c4
Nazar <tkachenkonazar2@gmail.com>
2021-11-04 18:58:56 +0200

    Создана модель Пользователя

---
+++ Fitness.BL/Model/User.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Fitness.BL.Model
+{
+    /// <summary>
+    /// User.
+    /// </summary>
+    
+    public class User
+    {
+        #region Properties
+        public string Name { get; }
+
+        public Gender Gender { get; }
+
+        public DateTime BirthDate { get; }
+
+        public double Weight { get; set; }
+
+        public double Height { get; set; }
+        #endregion
+        /// <summary>
+        /// Create new user.
+        /// </summary>
+        /// <param name="name">Name</param>
+        /// <param name="gender">Gender</param>
+        /// <param name="birthdate">Birthdate</param>
+        /// <param name="weight">Weight</param>
+        /// <param name="height">Height</param>
+        public User(string name, Gender gender, DateTime birthdate, double weight, double height)
+        {
+        #region Tests
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Invalid name user", nameof(name));
+            }
+
+            if (gender == null)
+            {
+                throw new ArgumentNullException("Invalid gender user", nameof(gender));
+            }
+
+            if (birthdate < DateTime.Parse("01.01.1900") || birthdate >= DateTime.Now )
+            {
+                throw new ArithmeticException ("Invalide birthdate", nameof(birthdate));
+            }
+
+            if (weight <= 0)
+            {
+                throw new ArgumentException ("Invalid weight", nameof(weight));
+            }
+
+            if(height <= 0)
+            {
+                throw new ArgumentException ("Invalid height", nameof(height));
+            }
+        #endregion
+
+            Name = name;
+            Gender = gender;
+            BirthDate = birthdate;
+            Weight = weight;
+            Height = height;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Fitness.CMD/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Fitness.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
