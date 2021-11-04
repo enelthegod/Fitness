b83496235c37dbf4025c44e144ff0a04524128e0
Nazar <tkachenkonazar2@gmail.com>
2021-11-04 18:58:56 +0200

    Создана модель Пользователя

---
+++ Fitness.BL/Model/Gender.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Fitness.BL.Model
+{
+    /// <summary>
+    /// Пол.
+    /// </summary>
+    [Serializable]
+    public class Gender
+    {
+     /// <summary>
+     /// Название.
+     /// </summary>
+        public string Name { get; }
+
+     /// <summary>
+     /// Создать новый пол.
+     /// </summary>
+     /// <param name="name">Имя пола. </param>
+
+        public Gender(string name)
+        {
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null", nameof(name));
+            }
+
+            Name = name;
+        }
+
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Fitness.CMD/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fitness.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My programm");
+            Console.WriteLine("Input user name-->");
+
+            var name = Console.ReadLine();
+            //TODO: |> Надо конструктор еще <|
+            Console.WriteLine("Input gender-->");
+            var gender = Console.ReadLine();
+
+            Console.WriteLine("Input -->");
+            var gender = Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
